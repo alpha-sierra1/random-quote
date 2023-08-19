0e4d3f34aed596cdc48dad3ce5ef1ba1f02f397d
AditiGi <aditisingh20144@gmail.com>
2023-08-19 23:09:06 +0530

    sahkhax

---
+++ reduce-method/script.js
@@ -0,0 +1,21 @@
+const array = [2, 4, 5, 6, 10];
+const sum = array.reduce((acc, current) => acc + current, 0);
+console.log(sum);
+
+const array1 = [3, 4, 4, 10, 12];
+const average = array1.reduce(
+  (acc, currentValue) => acc + currentValue / array1.length,
+  0
+);
+console.log(average);
+
+const array2 = [2, 4, 6, 3, 10, 15];
+const maximumValue = array2.reduce((acc, current) => {
+  if (current > acc) {
+    return current;
+  } else {
+    return acc;
+  }
+}, array2[0]);
+
+console.log(maximumValue);
